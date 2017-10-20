93c7873323e7c01e0dc62ffb22a13851f9b39ca5
Volodymyr Maystrovyy <maystrovyy@gmail.com>
2017-10-20 18:07:17 +0300

    Add bot.token file.

--- app/Module.scala
+++ app/Module.scala
@@ -4,13 +4,19 @@ import com.google.inject.AbstractModule
 import core.ReporterBot
 import services.{ApplicationTimer, AtomicCounter, Counter}
 
+import scala.io.Source
+
 class Module extends AbstractModule {
 
   override def configure(): Unit = {
     bind(classOf[Clock]).toInstance(Clock.systemUTC())
     bind(classOf[ApplicationTimer]).asEagerSingleton()
     bind(classOf[Counter]).to(classOf[AtomicCounter])
-    val reporterBot = new ReporterBot("451570869:AAEyTxoJ2oXkYccs5GHld6_CUQtXxDMC2PI").run()
+    val tokenPath = System.getProperty("user.dir") + "/conf/bot.token"
+    val token = scala.util.Properties
+      .envOrNone("BOT_TOKEN")
+      .getOrElse(Source.fromFile(tokenPath).getLines().mkString)
+    new ReporterBot(token).run()
   }
 
 }
